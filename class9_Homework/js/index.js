399a3a470e9124bc74026da183f7edd53d3579aa
Jonathan Boswell <jonathan@arborbridge.com>
2018-01-24 17:38:46 -0800

    Fixes :)

--- class9_Homework/js/index.js
+++ class9_Homework/js/index.js
@@ -4,6 +4,7 @@ var event ='click'
 $('#Wanna-see-where-I-wanna-go').on('click', displayImage);
 
 function displayImage(){
-    $('#Display_img').show('src','./images/maldives-resorts-gili-lankanfushi.jpg');
+    $('#Display_img').attr('src','./images/maldives-resorts-gili-lankanfushi.jpg');
     console.log("#yourButton was clicked");
+    $('#Display_img').show();
 }
