10410376ca1f54aa3645c60da0c9b5acdce4550d
Rosio Valdez <rosiovaldez@Rosios-MacBook-Pro.local>
2018-04-14 10:25:48 -0700

    js class 1 HW
    js class 1 HW

---
+++ js_w1/scripts.js
@@ -0,0 +1,37 @@
+//string
+var hola = "Hola to the world";
+hola += " lets groove together in our cars";
+console.log(hola);
+
+//number
+var age = 37;
+console.log(age);
+
+
+//boolean
+var dance = true;
+console.log(dance);
+
+//object
+var songs =
+{
+		'Kanye':['Famous','Fade' ],
+    'JayZ': ['Bam', 'Tom Ford'],
+    'MartinSolveig': ['Places','Intoxicated']
+};
+	console.log(songs.JayZ);
+	console.log(songs.MartinSolveig[1]);
+//function
+var carDance = function()
+{
+    console.log("Lets Groove");
+}
+carDance();
+
+
+function carDancin()
+{
+	console.log("hola");
+}
+
+carDancin();
